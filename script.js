3341933447e8879c9c12d19634a21e8aefa8532a
witehound <user@MACs-MacBook-Pro-2.local>
2022-12-27 13:09:24 +0100

    added color chna to h1

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-Please subscribe to my channel
+let t1 = gsap.timeline({});
+t1.from("logo", 1, {
+  y: -200,
+  opacity: 0,
+});
+
+t1.from("nav ul li", 1, {
+  y: -200,
+  stagger: 0.2,
+  opacity: 0,
+});
+
+t1.from(".menu", 1, {
+  y: -200,
+  stagger: 0.2,
+  opacity: 0,
+});
+
+t1.from("elmt", 1, {
+  y: -200,
+  stagger: 0.2,
+  opacity: 0,
+});
